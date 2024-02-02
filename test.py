40a724dd9755f75037d34b541fd3117d644f6bc1
Joshua Shiman <joshshiman@proton.me>
2024-02-02 13:59:45 -0500

    Test Webscraping

--- test.py
+++ test.py
@@ -4,9 +4,16 @@ from bs4 import BeautifulSoup
 URL = "https://github.com/IBM/itz-support-public/tree/main/IBM-Technology-Zone/IBM-Technology-Zone-Runbooks"
 page = requests.get(URL)
 
+soup = BeautifulSoup(page.text, 'html.parser')
 
-soup = BeautifulSoup(page.content, "html.parser")
+# Find all anchor tags with class "js-navigation-open" inside the file tree container
+file_links = soup.select('.js-navigation-open')
 
-links = soup.find_all(class_="Link--primary")
+# Extract file paths from the href attribute of each anchor tag
+file_paths = [link['href'] for link in file_links]
 
-print(page.text)
+# Filter out directories (ending with '/')
+file_paths = [path for path in file_paths if not path.endswith('/')]
+
+# Printing the list of file paths
+print(file_paths)
